f69a4b8de83f698e5532eabf69b98902234c0415
Mariotaku Lee <mariotaku.lee@gmail.com>
2014-01-14 21:42:53 +0800

    updated demo improved progress

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ RefreshNow
 ==========
 
 Google Now style pull to refresh
+
+![Demo](https://github.com/mariotaku/RefreshNow/raw/master/demo.gif)
--- library/src/org/mariotaku/refreshnow/widget/RefreshNowProgressIndicator.java
+++ library/src/org/mariotaku/refreshnow/widget/RefreshNowProgressIndicator.java
@@ -7,10 +7,12 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.animation.Interpolator;
 import fr.castorflex.android.smoothprogressbar.SmoothProgressBar;
 import fr.castorflex.android.smoothprogressbar.SmoothProgressDrawable;
 
@@ -30,7 +32,9 @@ public class RefreshNowProgressIndicator extends SmoothProgressBar implements IR
 		final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SmoothProgressBar, defStyle, 0);
 		final int color = a.getColor(R.styleable.SmoothProgressBar_spb_color, res.getColor(R.color.spb_default_color));
 		a.recycle();
-		setProgressColor(color);
+		final Config.Builder builder = new Config.Builder(context);
+		builder.progressColor(color);
+		setConfig(builder.build());
 		setMax(100);
 		setIndeterminate(false);
 		updateVisibility();
@@ -56,19 +60,15 @@ public class RefreshNowProgressIndicator extends SmoothProgressBar implements IR
 		updateVisibility();
 	}
 
-	public void setIndeterminateProgressBuilder(final SmoothProgressDrawable.Builder builder) {
-		setIndeterminateDrawable(builder.build());
-	}
-
-	public void setProgressColor(final int color) {
-		final ShapeDrawable shape = new ShapeDrawable();
-		shape.setShape(new RectShape());
-		shape.getPaint().setColor(color);
-		final ClipDrawable clipDrawable = new ClipDrawable(shape, Gravity.CENTER, ClipDrawable.HORIZONTAL);
-		setProgressDrawable(clipDrawable);
-		final SmoothProgressDrawable.Builder builder = new SmoothProgressDrawable.Builder(getContext());
-		builder.color(color);
-		setIndeterminateProgressBuilder(builder);
+	public void setConfig(final Config config) {
+		final Drawable progressDrawable = config.getProgressDrawable();
+		if (progressDrawable != null) {
+			setProgressDrawable(progressDrawable);
+		}
+		final Drawable indeterminateDrawable = config.getIndeterminateDrawable();
+		if (indeterminateDrawable != null) {
+			setIndeterminateDrawable(indeterminateDrawable);
+		}
 	}
 
 	private void updateVisibility() {
@@ -76,6 +76,117 @@ public class RefreshNowProgressIndicator extends SmoothProgressBar implements IR
 			setVisibility(VISIBLE);
 		} else {
 			setVisibility(getProgress() > 0 ? VISIBLE : GONE);
+		}
+	}
+
+	public static final class Config {
+
+		private final Drawable progressDrawable;
+		private final Drawable indeterminateDrawable;
+
+		private Config(final Drawable progressDrawable, final Drawable indeterminateDrawable) {
+			this.progressDrawable = progressDrawable;
+			this.indeterminateDrawable = indeterminateDrawable;
+		}
+
+		public Drawable getIndeterminateDrawable() {
+			return indeterminateDrawable;
+		}
+
+		public Drawable getProgressDrawable() {
+			return progressDrawable;
+		}
+
+		public static final class Builder {
+
+			private final SmoothProgressDrawable.Builder indeterminateDrawableBuilder;
+			private int progressColor;
+			private boolean changeProgressDrawable, changeIndeterminateDrawable;
+
+			public Builder(final Context context) {
+				indeterminateDrawableBuilder = new SmoothProgressDrawable.Builder(context);
+				progressColor(context.getResources().getColor(R.color.spb_default_color));
+			}
+
+			public Config build() {
+				final ShapeDrawable shape = new ShapeDrawable();
+				shape.setShape(new RectShape());
+				shape.getPaint().setColor(progressColor);
+				final ClipDrawable progressDrawable = new ClipDrawable(shape, Gravity.CENTER, ClipDrawable.HORIZONTAL);
+				return new Config(changeProgressDrawable ? progressDrawable : null,
+						changeIndeterminateDrawable ? indeterminateDrawableBuilder.build() : null);
+			}
+
+			public Builder changeIndeterminateDrawable(final boolean change) {
+				changeIndeterminateDrawable = change;
+				return this;
+			}
+
+			public Builder changeProgressDrawable(final boolean change) {
+				changeProgressDrawable = change;
+				return this;
+			}
+
+			public Builder indeterminateColor(final int color) {
+				indeterminateDrawableBuilder.color(color);
+				changeIndeterminateDrawable = true;
+				return this;
+			}
+
+			public Builder indeterminateColors(final int[] colors) {
+				indeterminateDrawableBuilder.colors(colors);
+				changeIndeterminateDrawable = true;
+				return this;
+			}
+
+			public Builder interpolator(final Interpolator interpolator) {
+				indeterminateDrawableBuilder.interpolator(interpolator);
+				changeIndeterminateDrawable = true;
+				return this;
+			}
+
+			public Builder mirrorMode(final boolean mirrorMode) {
+				indeterminateDrawableBuilder.mirrorMode(mirrorMode);
+				changeIndeterminateDrawable = true;
+				return this;
+			}
+
+			public Builder progressColor(final int color) {
+				progressColor = color;
+				changeProgressDrawable = true;
+				return this;
+			}
+
+			public Builder reversed(final boolean reversed) {
+				indeterminateDrawableBuilder.reversed(reversed);
+				changeIndeterminateDrawable = true;
+				return this;
+			}
+
+			public Builder sectionsCount(final int sectionsCount) {
+				indeterminateDrawableBuilder.sectionsCount(sectionsCount);
+				changeIndeterminateDrawable = true;
+				return this;
+			}
+
+			public Builder separatorLength(final int separatorLength) {
+				indeterminateDrawableBuilder.separatorLength(separatorLength);
+				changeIndeterminateDrawable = true;
+				return this;
+			}
+
+			public Builder speed(final float speed) {
+				indeterminateDrawableBuilder.speed(speed);
+				changeIndeterminateDrawable = true;
+				return this;
+			}
+
+			public Builder width(final int width) {
+				indeterminateDrawableBuilder.width(width);
+				changeIndeterminateDrawable = true;
+				return this;
+			}
+
 		}
 	}
 }
